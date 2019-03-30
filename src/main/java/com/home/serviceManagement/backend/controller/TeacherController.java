7e75e16c78846a869e06552930e4a23fd1b10694
Ranga Lankathilaka <lankathilakamrp@gmail.com>
2019-03-30 16:49:07 +0530

    query dsl join

--- src/main/java/com/home/serviceManagement/backend/controller/TeacherController.java
+++ src/main/java/com/home/serviceManagement/backend/controller/TeacherController.java
@@ -1,10 +1,7 @@
 package com.home.serviceManagement.backend.controller;
 
-import java.util.ArrayList;
-
 import java.util.List;
 
-import com.home.serviceManagement.backend.dto.joinDto.TeacherTicketDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.home.serviceManagement.backend.dto.TeacherDTO;
-import com.home.serviceManagement.backend.entity.Teacher;
+import com.home.serviceManagement.backend.dto.joinDto.TeacherTicketDTO;
 import com.home.serviceManagement.backend.service.TeacherService;
 
 @RestController
